835f831d6fc54bb06b6c790cdc22e451c4ce8dc9
Killian James <james.k@etoncollege.org.uk>
2022-03-02 20:36:18 +0000

    Part 2 Done, info.plist still needed

--- MusicByLocation/LocationHandler.swift
+++ MusicByLocation/LocationHandler.swift
@@ -8,10 +8,10 @@
 import Foundation
 import CoreLocation
 
-class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject {
+class LocationHandler: NSObject, CLLocationManagerDelegate {
     let manager = CLLocationManager()
     let geocoder = CLGeocoder()
-    @Published var lastKnownLocation: String = ""
+    weak var stateController: StateController?
     
     override init() {
         super.init()
@@ -30,10 +30,10 @@ class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject {
         if let firstLocation = locations.first {
             geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in
                 if error != nil {
-                    self.lastKnownLocation = "Could not perform lookup of location from coordiante information"
+                    self.stateController?.lastKnownLocation = "Could not perform lookup of location from coordiante information"
                 } else {
                     if let firstPlacemark = placemarks?[0] {
-                        self.lastKnownLocation = firstPlacemark.locality ?? "Could not find locality"
+                        self.stateController?.lastKnownLocation = firstPlacemark.getLocationBreakdown()
                     }
                 }
             })
@@ -41,6 +41,6 @@ class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject {
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        lastKnownLocation = "Error finding location"
+        self.stateController?.lastKnownLocation = "Error finding location"
     }
 }
