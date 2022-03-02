0a092e7ea1f438680f83ee64602e6ced2f89b14c
Killian James <james.k@etoncollege.org.uk>
2022-03-02 15:24:45 +0000

    Part 1 done except info.plist file

---
+++ MusicByLocation/LocationHandler.swift
@@ -0,0 +1,46 @@
+//
+//  LocationHandler.swift
+//  MusicByLocation
+//
+//  Created by James, Killian (NA) on 02/03/2022.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject {
+    let manager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    @Published var lastKnownLocation: String = ""
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestAuthorisation() {
+        manager.requestWhenInUseAuthorization()
+    }
+    
+    func requestLocation() {
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let firstLocation = locations.first {
+            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in
+                if error != nil {
+                    self.lastKnownLocation = "Could not perform lookup of location from coordiante information"
+                } else {
+                    if let firstPlacemark = placemarks?[0] {
+                        self.lastKnownLocation = firstPlacemark.locality ?? "Could not find locality"
+                    }
+                }
+            })
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        lastKnownLocation = "Error finding location"
+    }
+}
