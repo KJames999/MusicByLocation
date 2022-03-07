ac1cce6157fbeb4bfe984bf60c098870ef120f2c
Killian James <james.k@etoncollege.org.uk>
2022-03-07 15:30:27 +0000

    Part 2 Done, can't get the app icon it just won't allow me to select it from one drive

--- MusicByLocation/StateController.swift
+++ MusicByLocation/StateController.swift
@@ -8,9 +8,15 @@
 import Foundation
 
 class StateController: ObservableObject {
-    @Published var lastKnownLocation: String = ""
-    @Published var artistNames: String = ""
     let locationHandler: LocationHandler = LocationHandler()
+    let iTunesAdaptor = ITunesAdapor()
+    @Published var artistsByLocation: String = ""
+    
+    @Published var lastKnownLocation: String = "" {
+        didSet {
+            iTunesAdaptor.getArtists(search: lastKnownLocation, completion: updateArtistsByLocation)
+        }
+    }
     
     func findMusic() {
         locationHandler.requestLocation()
@@ -21,39 +27,10 @@ class StateController: ObservableObject {
         locationHandler.requestAuthorisation()
     }
     
-    func getArtists() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=Lionel%20Richie&entity=musicArtist")
-        else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let data = data {
-                if let response = self.parseJson(json: data) {
-                    let names = response.results.map {
-                        return $0.name
-                    }
-                    
-                    DispatchQueue.main.async {
-                        self.artistNames = names.joined(separator: ", ")
-                    }
-                    
-                }
-            }
-        }.resume()
-    }
-    
-    func parseJson(json: Data) -> ArtistResponse? {
-        let decoder = JSONDecoder()
-        
-        if let artistResponse = try? decoder.decode(ArtistResponse.self, from: json) {
-            return artistResponse
-        } else {
-            print("Error decoding JSON")
-            return nil
+    func updateArtistsByLocation(artists: [Artist]?) {
+        let names = artists?.map { return $0.name }
+        DispatchQueue.main.async {
+            self.artistsByLocation = names?.joined(separator: ", ") ?? "Error finding Artists from your Location"
         }
     }
 }
